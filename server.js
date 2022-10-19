eb3aed50c6da67da21feafda6dc85400e0f0211c
TalysonSoares <talysonsoares40@gmail.com>
2022-10-19 10:28:11 -0300

    criando servidor

--- server.js
+++ server.js
@@ -4,14 +4,15 @@ const host = 'localhost';
 const port = '8000';
 
 //função que é executada a cada request
-
 const eachRequest = (request, response) => {
     //Definindo o tipo de resposta para o json
     response.setHeader('Content-Type', 'application/json')
 
     //definindo conteudo da resposta
-    let content = JSON.stringify()
+    let content = JSON.stringify("Testando")
     
     //respondendo pro httpClient
     response.end(content);
 };
+
+const server = http.createServer(eachRequest);
