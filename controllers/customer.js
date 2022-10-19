202fee60585a7fa5ec2b8b9ef3161ba55704e3f2
TalysonSoares <talysonsoares40@gmail.com>
2022-10-19 10:07:33 -0300

    criando funções

---
+++ controllers/customer.js
@@ -0,0 +1,15 @@
+function add(){
+    return "Cadastrando Cliente"
+}
+
+function list() {
+    return "Listando Cliente"
+}
+
+function uptade() {
+    return "Editando Cliente"
+} 
+
+function remove() {
+    return "Excluindo Cliente"
+}
---
+++ controllers/vehicle.js
@@ -0,0 +1,15 @@
+function add(){
+    return "Cadastrando Veiculo"
+}
+
+function list() {
+    return "Listando Veiculo"
+}
+
+function uptade() {
+    return "Editando Veiculo"
+} 
+
+function remove() {
+    return "Excluindo Veiculo"
+}
