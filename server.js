09de4dc526244daa4292fdae835a5a2a6bc23ee2
TalysonSoares <talysonsoares40@gmail.com>
2022-10-19 10:22:24 -0300

    criando função que é executada a cada request e definindo conteudo da resposta

--- server.js
+++ server.js
@@ -2,3 +2,11 @@ const http = require('http');
 
 const host = 'localhost';
 const port = '8000';
+
+//função que é executada a cada request
+
+const eachRequest = (request, response) => {
+    let content = JSON.stringify()
+    
+    response.end();
+};
