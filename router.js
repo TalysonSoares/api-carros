c25bc9a8029fb562d4200463c564524b9bbf10c7
TalysonSoares <talysonsoares40@gmail.com>
2022-10-19 11:09:00 -0300

    Criando const routes

--- router.js
+++ router.js
@@ -1,3 +1,16 @@
+const routes = {
+    '/customers': {
+        'GET': 'Cadastro do cliente',
+        'POST': 'listagem do cliente',
+    },
+    '/vehicles': {
+        'GET': 'Cadastro do Veiculo',
+        'POST': 'listagem do veiculos',
+    },
+}
+
+
+
 function router(url, method) {
     //a url representa qual controller a gente precisa
     //o method representa qual fuunction do controller vai ser executada
